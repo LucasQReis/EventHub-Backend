8f76a90bae77253e53248a234a906199d3dabb81
LucasQReis <lqrreis@furb.br>
2025-01-07 14:50:44 -0300

    feat: setting up database configuration

--- src/main/java/com/api/EventHub/EventHubApplication.java
+++ src/main/java/com/api/EventHub/EventHubApplication.java
@@ -2,9 +2,8 @@ package com.api.EventHub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
+@SpringBootApplication
 public class EventHubApplication {
 
 	public static void main(String[] args) {
